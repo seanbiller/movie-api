e60735915ca0c365b6eb81ca619fae87f65e1c2b
Sean Biller <seanmbiller@gmail.com>
2019-12-11 15:00:07 -0500

    Initial setup

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+const allConfigs = require('../config/sequelize')
+const MoviesModel = require('./rebels')
+
+const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
+const config = allConfigs[environment]
+
+const connection = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect,
+})
+
+const Movies = MoviesModel(connection, Sequelize)
+
+module.exports = {
+  Movies,
+}
